aeb059309d3795aca02211ac9246e7138f44fcc5
Bruno Galvao <bruno_galvao@outlook.com>
2023-04-21 19:04:22 -0300

    fix - estrutura inicial

---
+++ src/View/Home/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121214',
+    padding: 42,
+  },
+  titulo: {
+    color: '#FFF',
+    fontSize: 32,
+    marginTop: 32,
+    textAlign: 'center',
+  }
+});
